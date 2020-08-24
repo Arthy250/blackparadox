64615150b2c0ba8349c5e62b8e03501f989a890f
Arthy250 <arthy250@gmail.com>
2020-08-23 20:28:04 -0500

    ajustes index

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
     captions: false,
     // controls: false
   });
+
+  // Detectar el ancho del diamante y usar el mismo valor en height
+  var dn_width = $(".diamante").width();
+  $(".diamante").css({
+    height: dn_width
+  });
+
+  // Agregar clase al hacer scroll
+  var slider =  $('#slider').height();
+  $(window).scroll(function(){
+    var scroll = $(window).scrollTop();
+    if (scroll >= $('#nosotros').offset().top - 100) {
+      $('#nav').addClass("nav-overlay");
+    } else{
+      $('#nav').removeClass("nav-overlay");
+    }
+  });
 });
